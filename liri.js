46aab4e3c9ac0c5a1f09b63a251d5035dc00c8f6
FrankieFigz <fjvannini@gmail.com>
2019-10-15 21:13:48 -0400

    Added clean up methods to make code more compact

--- liri.js
+++ liri.js
@@ -1,45 +1,51 @@
-// Where to place the API keys
-require("dotenv").config();
-// Adding the keys import class
-var keys = require("./keys.js");
-// Package requirements
-var axios = require('axios');
-var inquirer = require('inquirer');
-var Spotify = require('node-spotify-api');
-var spotify = new Spotify(keys.spotifyKeys);
+  // Where to place the API keys
+  require("dotenv").config();
 
-// fs is a core Node package for reading and writing files
-var fs = require("fs");
+  // Adding the keys import class
+  var keys = require("./keys.js");
 
-// Create a "Prompt" with a series of questions.
-inquirer.prompt([
-    // Here are the possible LIRI options.
-    {
-      type: "list",
-      message: "Which Pokemon do you choose?",
-      choices: ["concert-this", "spotify-this-song", "movie-this","do-what-it-says"],
-      name: "choice"
-    }
-  ]).then(function(inquirerResponse) {
-    
-    // Depending on the user selection call a specific function
-    switch(inquirerResponse.choice)
-    {
-        case 'movie-this':
-            movieSearch("");
-            break;
-        case 'concert-this':
-            eventSearch("");
-            break;
-        case 'spotify-this-song':
-            musicSearch("");
-            break;
-        case 'do-what-it-says':
-            doThis();
-            break;
-    }
+  // Package requirements
+  var axios = require('axios');
+  var inquirer = require('inquirer');
+  var Spotify = require('node-spotify-api');
+  var spotify = new Spotify(keys.spotifyKeys);
+  var moment = require("moment");
 
-  });
+  // fs is a core Node package for reading and writing files
+  var fs = require("fs");
+
+  // Create a "Prompt" with a series of questions.
+  function startPrompt(){
+    inquirer.prompt([
+      // Here are the possible LIRI options.
+      {
+        type: "list",
+        message: "Which Pokemon do you choose?",
+        choices: ["concert-this", "spotify-this-song", "movie-this","do-what-it-says"],
+        name: "choice"
+      }
+    ]).then(function(inquirerResponse) {
+      
+      // Depending on the user selection call a specific function
+      switch(inquirerResponse.choice)
+      {
+          case 'movie-this':
+              movieSearch("inquirerInput");
+              break;
+          case 'concert-this':
+              eventSearch("inquirerInput");
+              break;
+          case 'spotify-this-song':
+              musicSearch("inquirerInput");
+              break;
+          case 'do-what-it-says':
+              doThis();
+              break;
+      }
+
+    });
+
+  }
 
   function workMovie(movie){
     var queryMovie = movie;
@@ -89,9 +95,32 @@ inquirer.prompt([
         });
   };
 
-  function movieSearch(p){
+  function workEvent(bandName){
 
-    if(p == ""){
+    var response = bandName;
+    var artistQuery = response.replace(/\s/g,'+').toLowerCase();
+    var userQuery = "https://rest.bandsintown.com/artists/" + artistQuery + "/events?app_id=codingbootcamp";
+  
+      axios.get(userQuery)
+      .then(function(response) {
+        if (response.data !== '[]') {
+          for (var i = 0; i < response.data.length; i++){
+            console.log("Venue: " + response.data[i].venue.name); 
+            console.log("Location: " + response.data[i].venue.city + ", " + response.data[i].venue.region + ", " + response.data[i].venue.country);
+            var date = moment(response.data[i].datetime).format('MM/DD/YYYY');
+            console.log("Date: " + date);        
+            console.log("-----------------------------------------------------");
+          }
+        }
+        else {
+          console.log('No concerts found. Try again.');
+        }
+      });  
+
+  };
+  
+  function movieSearch(p){
+    if(p == "inquirerInput"){
       inquirer.prompt([
         {
           type: "text",
@@ -107,8 +136,8 @@ inquirer.prompt([
   };
 
   function eventSearch(p){
-
-    inquirer.prompt([
+    if(p == "inquirerInput"){
+      inquirer.prompt([
         // Here are the possible LIRI options.
         {
           type: "text",
@@ -116,83 +145,63 @@ inquirer.prompt([
           name: "band"
         }
       ]).then(function(inquirerResponse) {
-        
-        var queryBand = inquirerResponse.band;
-        queryBand = queryBand.replace(/\s/g,"+");
-
-        var userQuery = "https://rest.bandsintown.com/artists/" +  queryBand + "/events?app_id=codingbootcamp";
-            
-        // Optionally the request above could also be done as
-        axios.get(userQuery)        
-        .then(function (response) {
-            // console.log(response);
-            for(var key in response.data){
-                console.log(response.data[key].venue);
-                console.log("Name of venue : " + response.data[key].venue.name);
-                console.log("Location of venue " + response.data[key].venue.city + " , " + response.data[key].venue.country);
-                console.log("Date of concert : " + response.data[key].datetime);
-                // console.log(" ");
-            }
-
-        })
-        .catch(function (error) {
-            console.log(error);
-        })
-        .then(function () {
-            // always executed
-        });  
-        
+        workEvent(inquirerResponse.band);
       });
-   
-  }
-    function musicSearch(p) {
-        inquirer.prompt([
-          {
-            type: "input",
-            name: "song",
-            message: "What song would you like to spotify?"
-          }
-        ])
-        .then(function(response){
-          workMusic(response);
-          })
-      };
+    } else{
+      workEvent(p);
+    }
+  };
 
-      function doThis(){
+  function musicSearch(p) {
+    if(p == 'inquirerInput'){
+      inquirer.prompt([
+        {
+          type: "input",
+          name: "song",
+          message: "What song would you like to spotify?"
+        }])
+          .then(function(inquirerResponse){
+            workMusic(inquirerResponse.song);
+        })
+    } else {
+      workMusic(p);
+    }
+  };
+
+  function doThis(){
         // This block of code will read from the "movies.txt" file.
         // It's important to include the "utf8" parameter or the code will provide stream data (garbage)
         // The code will store the contents of the reading inside the variable "data"
     
-        fs.readFile("random.txt", "utf8", function(error, data) {
+    fs.readFile("random.txt", "utf8", function(error, data) {
         // If the code experiences any errors it will log the error to the console.
-        if (error) {
-            return console.log(error);
-        }
-    
-        // We will then print the contents of data
-        console.log(data);
-    
-        // Then split it by commas (to make it more readable)
-        var dataArr = data.split(",");
-    
-        // We will then re-display the content as an array for later use.
-        console.log(dataArr[0]);
-        console.log(dataArr[1]);
+      if (error) 
+        return console.log(error);
 
-        switch(dataArr[0])
-        {
-            case 'movie-this':
-                movieSearch(dataArr[1]);
-                break;
-            case 'concert-this':
-                eventSearch(dataArr[1]);
-                break;
-            case 'spotify-this-song':
-                musicSearch(dataArr[1]);
-                break;
-        }
+      // We will then print the contents of data
+      console.log(data);
     
-        });
-        
-      };  
-      
+      // Then split it by commas (to make it more readable)
+      var dataArr = data.split(",");
+    
+      // We will then re-display the content as an array for later use.
+      console.log(dataArr[0]);
+      console.log(dataArr[1]);
+
+      switch(dataArr[0])
+      {
+        case 'movie-this':
+          movieSearch(dataArr[1]);
+          break;
+        case 'concert-this':
+          eventSearch(dataArr[1]);
+          break;
+        case 'spotify-this-song':
+          musicSearch(dataArr[1]);
+          break;
+      }
+    });       
+  };  
+
+  // Starts the LIRI bot sequence
+  startPrompt();
